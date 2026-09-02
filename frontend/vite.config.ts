419fd9641ef8e22e0b6db5f91508b2a43b6b2592
Vivek Kumar Nishad <vivek271cs@gmail.com>
2026-09-03 00:29:18 +0530

    feat: optimize for seamless 1-click Vercel hosting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './',
+  base: process.env.GITHUB_PAGES ? '/SIH_PS-3dUlpin/' : '/',
   plugins: [
     tailwindcss(),
     react()
